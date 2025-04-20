e84678c833be042a3968725f5351f1f76726fe31
luu-9798 <trungluu9798@gmail.com>
2025-04-20 16:03:35 -0400

    Create an Identifiable struct

---
+++ ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct Item: Identifiable {
+    let id  = UUID()
+    let name: String
+}
